6d160a4a0371c48e1c056c039c106971bdf18585
jimmy <1559810853@proton.me>
2025-12-12 19:56:43 +0800

    新增ChatService

--- src/server/chatserver.cpp
+++ src/server/chatserver.cpp
@@ -1,7 +1,8 @@
 #include"chatserver.hpp"
 #include<functional>
-
-
+#include"json.hpp"
+#include"chatservice.hpp"
+using json=
 ChatServer::ChatServer(EventLoop* loop,
     const InetAddress& listenAddr,//IP+port
     const string& nameArg)//服务器名字
@@ -50,7 +51,12 @@ void ChatServer::onMessage(const TcpConnectionPtr&conn,//连接
                 Timestamp time)//接收数据的时间信息
 {
     string buf=buffer->retrieveAllAsString();
-    cout<<"recv data:"<<buf<<" time:"<<time.toString()<<endl;
-    conn->send(buf);
+    //数据的反序列化
+    json js =json::parse(buf);
+    // 达到的目的：完全解耦网络模块的代码和业务模块的代码
+    //通过js["msgid"]获取=》业务handler=》conn js time
+    auto msgHandler=ChatService::instance()->getHandler(js["msgid"].get<int>());
+    //回调消息绑定好的事件处理器，来执行相应的业务处理
+    msgHandler(conn,js,time);
 }
     
